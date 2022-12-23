3226de051c05f8a3e5057ac92ad8b746dd920059
prafulpatel16 <praful1611@outlook.com>
2022-12-23 16:18:38 -0600

    remote backend added

--- providers.tf
+++ providers.tf
@@ -1,15 +1,20 @@
 # Terraform Block
 terraform {
-  backend "local" {
-    path = "static-web-statefile/terraform.tfstate" //local backend to store the state locally
+  cloud {
+    organization = "prafect"
+
+    workspaces {
+      name = "static-web-circleci"
+    }
   }
+}
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "4.48.0"
     }
   }
-}
+
 
 # Provider Block
 provider "aws" {
